adc9c28d86617c61187dd070aecd1c1ae9583587
baobuia1 <baobuia1.py@gmail.com>
2024-02-27 11:41:24 +0700

    Added depth

--- engine.py
+++ engine.py
@@ -2,18 +2,24 @@ import const
 import chess
 
 class Engine(chess.Board):
-    def __init__(self,fen='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',chess960=False,side=-1):
+    def __init__(self,fen='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',chess960=False,side=-1,depth=5):
         super().__init__(fen)
         self.side = side
+        self.depth = depth
 
-    def search(self) -> str:
+    def search(self,current_depth=0):
         good_moves = {}
         for move in list(self.legal_moves):
             self.push(move)
-            good_moves[str(move)] = self.evaluate()
+            if current_depth == self.depth:
+                self.pop()
+                return self.evaluate()
+            good_moves[str(move)] = self.search(current_depth+1)
             self.pop()
+        print('\n')
+        print(move)
         print(good_moves)
-        if self.side == -1:
+        if current_depth%2 == 1:
             return min(good_moves, key=good_moves.get)
         else:
             return max(good_moves, key=good_moves.get)
@@ -23,24 +29,24 @@ class Engine(chess.Board):
         piece_map = self.piece_map()
         for square in piece_map:
             material_of_square = 0
-            side = 0
+            side_of_piece = 0
             if str(piece_map[square]).upper() == str(piece_map[square]):
                 material_of_square = 1
-                side = -1
+                side_of_piece = -1
             else:
                 material_of_square = -1
-                side = 1
+                side_of_piece = 1
             if str(piece_map[square]).lower() == 'p':
-                material_of_square *= const.PAWN * const.mg_pawn_table[::side][square//8][square%8]
+                material_of_square *= const.PAWN * const.mg_pawn_table[::side_of_piece][square//8][square%8]
             elif str(piece_map[square]).lower() == 'b':
-                material_of_square *= const.BISHOP * const.mg_bishop_table[::side][square//8][square%8]
+                material_of_square *= const.BISHOP * const.mg_bishop_table[::side_of_piece][square//8][square%8]
             elif str(piece_map[square]).lower() == 'n':
-                material_of_square *= const.KNIGHT * const.mg_knight_table[::side][square//8][square%8]
+                material_of_square *= const.KNIGHT * const.mg_knight_table[::side_of_piece][square//8][square%8]
             elif str(piece_map[square]).lower() == 'r':
-                material_of_square *= const.ROOK * const.mg_rook_table[::side][square//8][square%8]
+                material_of_square *= const.ROOK * const.mg_rook_table[::side_of_piece][square//8][square%8]
             elif str(piece_map[square]).lower() == 'q':
-                material_of_square *= const.QUEEN * const.mg_queen_table[::side][square//8][square%8]
+                material_of_square *= const.QUEEN * const.mg_queen_table[::side_of_piece][square//8][square%8]
             elif str(piece_map[square]).lower() == 'k':
-                material_of_square *= const.KING * const.mg_king_table[::side][square//8][square%8]
+                material_of_square *= const.KING * const.mg_king_table[::side_of_piece][square//8][square%8]
             evaluation += material_of_square
         return evaluation
